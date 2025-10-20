0f090279b4706a5cb36e4d1ad67678076d6c1ff7
ZilanCH <mc.zilan.ch@gmail.com>
2025-10-20 17:07:43 +0000

    Test

--- data.js
+++ data.js
@@ -3,13 +3,12 @@ const data = {
     about: "Wir sind ZilanDEV, eine Gaming-Community und Entwicklergruppe, die sich leidenschaftlich für die Erstellung von hochwertigen Softwarelösungen einsetzt. Unser Ziel ist es, innovative und unterhaltsame Erlebnisse zu schaffen, die Menschen weltweit begeistern.",
     news: [
         { date: "12.09.2025", title: "WebV1 - ZilanDEV", content: "Wir haben die Webseite aufgesetzt :D" },
+        { date: "2.10.2025", title: "ZilanOS", content: "Wir haben mit der Entwicklung von ZilanOS begonnen." }
     ],
     links: [
         { name: "Discord", link: "https://dc.tth-projects.de", img: "img/discord.webp" },
-        { name: "Miyako", link: "https://miyako.zilan.dev", img: "img/miyako.jpg" },
-        { name: "Drakurim", link: "https://drakurim.zilan.dev", img: "img/drakurim.jpg" }, 
-        { name: "GitHub", link: "https://github.com/zilanch", img: "img/discord.webp" },
-        { name: "S4riX", link: "https://s4rix.zilan.dev", img: "img/s4rix.png" },
-        { name: "Theredstonee-Projects", link: "https://www.theredstonee.de", img: "img/theredstonee.webp" },
+        { name: "Miyako", link: "https://miyako.zilan.dev", img: "img/miyako.webp" },
+        { name: "GitHub", link: "https://github.com/zilanch", img: "img/zilan_logo.webp" },
+        { name: "Sarikaze", link: "https://s4rix.zilan.dev", img: "img/s4rix.webp" },
     ]
 };
